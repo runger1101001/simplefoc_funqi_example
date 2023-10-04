46869583089353edd69129b95a053c43e7f56f5d
Richard Unger <richard.unger@nexum.com>
2023-10-04 15:47:17 +0200

    init is working but open loop movement is not

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,25 @@
 
 #define FUNQI_PIN_NEOPIXEL PA10
 
+#define FUNQI_PIN_INXL  PB0
+#define FUNQI_PIN_INXH  PA14
+
+#define FUNQI_PIN_MOSI PB5
+#define FUNQI_PIN_MISO PB4
+#define FUNQI_PIN_SCK  PB3
+#define FUNQI_PIN_CS   PA15
+
 
 Adafruit_NeoPixel pix = Adafruit_NeoPixel(1, FUNQI_PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 
 STSPIN32G4 driver = STSPIN32G4();
-BLDCMotor motor = BLDCMotor(7);
-
-
+BLDCMotor motor = BLDCMotor(11);
 
 
 
 void printDriverStatus();
 
-
+uint32_t ts = 0;
 
 void setup() {
     Serial.begin(115200);
@@ -28,9 +34,15 @@ void setup() {
     delay(5000);
     Serial.println("Welcome to FunQi Driver Firmware");
 
-    Serial.println("Initializing CORDIC");
-    if (!SimpleFOC_CORDIC_Config())
-        Serial.println("CORDIC init failed");
+    // configure some pins
+    pinMode(FUNQI_PIN_INXL, OUTPUT);
+    pinMode(FUNQI_PIN_INXH, OUTPUT);
+    digitalWrite(FUNQI_PIN_INXL, LOW); // disable 4th channel for now
+    digitalWrite(FUNQI_PIN_INXH, LOW);
+
+    // Serial.println("Initializing CORDIC");
+    // if (!SimpleFOC_CORDIC_Config())
+    //     Serial.println("CORDIC init failed");
 
     Serial.println("Initializing LED");
     pix.begin();
@@ -38,10 +50,10 @@ void setup() {
     pix.setPixelColor(0, 95, 6, 87);
     pix.show();
 
-    Serial.println("Initializing Driver");
+    Serial.println("Initializing STSPIN");
     driver.voltage_power_supply = 12.0f;
     driver.voltage_limit = 10.0f;
-    driver.init();
+    driver.initSTSPIN();
     delay(1);
 
     Serial.print("Driver ready: ");
@@ -57,30 +69,64 @@ void setup() {
     powmng.vcc_val = 0b01;
     driver.setPowMngRegister(powmng);
     Serial.println("Set VCC to 10V");
-    driver.clearFaults();
-    printDriverStatus();
     driver.lock();
+    delayMicroseconds(50);
+    driver.clearFaults();
+    delayMicroseconds(50);
+    printDriverStatus();
+    Serial.print("Driver ready: ");
+    Serial.println(driver.isReady()?"true":"false");
+    Serial.print("Driver fault: ");
+    Serial.println(driver.isFault()?"true":"false");    
+
+    // init PWM
+    Serial.println("Initializing PWM");
+    driver.init();
+    delayMicroseconds(50);
+    Serial.print("Driver ready: ");
+    Serial.println(driver.isReady()?"true":"false");
+    Serial.print("Driver fault: ");
+    Serial.println(driver.isFault()?"true":"false");
+    printDriverStatus();
+    driver.clearFaults();
 
     Serial.println("Initializing Motor");
     motor.voltage_limit = driver.voltage_limit / 2.0f;
     motor.controller = MotionControlType::velocity_openloop;
+    motor.foc_modulation = FOCModulationType::SinePWM;
     motor.linkDriver(&driver);
     motor.init();
-
-    motor.enable();
     Serial.println("Motor enabled");
 
-    pix.setPixelColor(0, 0, 102, 0);
+    pix.setPixelColor(0, 0, 52, 0);
     pix.show();
     Serial.println("Setup complete");
+    ts = millis();
+    motor.target = 5.0f;
 }
+
+
+int count = 0;
 
 void loop(){
     // CORDIC test
     // float a = millis() / 159.235668789808917f / 20;
     // Serial.println(_sin(a),4);
     // delay(1000);
-    motor.move(5.0f);
+
+    uint32_t now = millis();
+    if (now-ts>1000) {
+        Serial.print("It/s: ");
+        Serial.print(count);
+        Serial.print("   Fault: ");
+        Serial.print(driver.isFault()?"true":"false");
+        Serial.print("   Angle: ");
+        Serial.println(motor.shaft_angle);        
+        count = 0;
+        ts = now;
+    }
+    count++;
+    motor.move();
     delayMicroseconds(100);
 }
 
