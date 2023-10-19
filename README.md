310ed36113b3fe1d62d3f7dfc9f8540b9ae2ac59
Richard Unger <richard.unger@nexum.com>
2023-10-19 13:01:02 +0200

    commit initial closed loop code

---
+++ README.md
@@ -0,0 +1,27 @@
+# SimpleFOC field stack example
+
+Example firmware for driving a BLDC motor (not stepper) with 3 phases on the Funqi board module for the Field Stack driver.
+
+It's also an example of how to use the STSPIN32G4 from PlatformIO.
+
+Features:
+
+- PlatformIO project
+- Board configuration for working with generic STSPIN32G4 boards
+- uses SimpleFOC and SimpleFOC drivers libraries
+- Controls a BLDC motor
+- Serial control via USB Serial and SimpleFOC's Commander class
+- STSPIN32G4 driver for interacting with driver stage
+- MT6835 driver for sensor
+- STM32 CORDIC accellerated trig functions
+- NeoPixel for status indication
+
+
+## Software setup
+
+Clone this repository, then switch to the lib folder and clone the dev branches of the SimpleFOC library and the SimpleFOC drivers library to that location.
+
+Open the folder in PlatformIO.
+
+Make sure your STM32 platform files are up-to-date.
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "drivers/stspin32g4/STSPIN32G4.h"
 #include "utilities/stm32math/STM32G4CORDICTrigFunctions.h"
 #include "Adafruit_NeoPixel.h"
+#include "encoders/mt6835/MagneticSensorMT6835.h"
 
 
 #define FUNQI_PIN_NEOPIXEL PA10
@@ -22,7 +23,11 @@ Adafruit_NeoPixel pix = Adafruit_NeoPixel(1, FUNQI_PIN_NEOPIXEL, NEO_GRB + NEO_K
 STSPIN32G4 driver = STSPIN32G4();
 BLDCMotor motor = BLDCMotor(11);
 
+SPIClass sensorSPI = SPIClass(FUNQI_PIN_MOSI, FUNQI_PIN_MISO, FUNQI_PIN_SCK);
+MagneticSensorMT6835 sensor = MagneticSensorMT6835(FUNQI_PIN_CS);
 
+Commander commander;
+void onMotor(char* cmd) { commander.motor(&motor, cmd); }
 
 void printDriverStatus();
 
@@ -66,9 +71,10 @@ void setup() {
     // STSPIN32G4PowMng powmng = driver.getPowMngRegister();
     // Serial.print("VCC voltage: ");
     // Serial.println(powmng.vcc_val);
+    // Serial.println("Setting VCC to 10V");
     // powmng.vcc_val = 0b01;
     // driver.setPowMngRegister(powmng);
-    // Serial.println("Set VCC to 10V");
+    // Serial.println("Locking driver...");
     // driver.lock();
     // driver.clearFaults();
     // printDriverStatus();
@@ -77,14 +83,34 @@ void setup() {
     // Serial.print("Driver fault: ");
     // Serial.println(driver.isFault()?"true":"false");    
 
+    Serial.println("Initializing Sensor");
+    sensor.init(&sensorSPI);
+    MT6835Options1 options1 = sensor.getOptions1();
+    options1.uvw_off = 0b1;
+    sensor.setOptions1(options1);
+    sensor.setABZEnabled(true);
+    sensor.setABZResolution(16000-1); // sets 16000 PPR
+    Serial.println("Sensor initialized, start angle: ");
+    Serial.println(sensor.getAngle());
 
     Serial.println("Initializing Motor");
     motor.voltage_limit = driver.voltage_limit / 2.0f;
-    motor.controller = MotionControlType::velocity_openloop;
+    motor.controller = MotionControlType::torque;
+    motor.torque_controller = TorqueControlType::voltage;
     motor.foc_modulation = FOCModulationType::SinePWM;
+    motor.motion_downsample = 4;
     motor.linkDriver(&driver);
+    motor.linkSensor(&sensor);
     motor.init();
     Serial.println("Motor enabled");
+
+    pix.setPixelColor(0, 0, 52, 46);
+    pix.show();
+    Serial.println("Motor calibration...");
+    motor.initFOC();
+
+    Serial.println("Initializing comms");
+    commander.add('M', onMotor, "Motor 1");
 
     pix.setPixelColor(0, 0, 52, 0);
     pix.show();
@@ -92,6 +118,8 @@ void setup() {
     ts = millis();
     motor.target = 5.0f;
 }
+
+
 
 
 int count = 0;
@@ -104,13 +132,14 @@ void loop(){
         Serial.print("  Fault: ");
         Serial.print(driver.isFault()?"true":"false");
         Serial.print("  Angle: ");
-        Serial.println(motor.shaft_angle);
+        Serial.println(sensor.getAngle());
         count = 0;
         ts = now;
     }
     count++;
     motor.move();
-    delayMicroseconds(100);
+    motor.loopFOC();
+    commander.run();
 }
 
 
